7f153ddc60efd404fce532e7e05496fc44f20f0e
Gihanvs98 <darkcreationsl98@gmail.com>
2025-04-01 21:45:38 +0530

    category route

--- route/CategoryRoute.js
+++ route/CategoryRoute.js
@@ -3,6 +3,10 @@ const CategoryController = require('../controller/CategoryController');
 
 const router = express.Router();
 
-
+router.post('/create-category', CategoryController.createCategory);
+router.put('/update-category/:id', CategoryController.updateCategory);
+router.delete('/delete-category/:id', CategoryController.deleteCategory);
+router.get('/find-category-by-id/:id', CategoryController.findCategoryById);
+router.get('/find-all-categories', CategoryController.findAllCategories);
 
 module.exports = router;
