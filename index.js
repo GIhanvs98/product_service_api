44339975af9a3d2000a70e1ae3bc9997800c2bd1
Gihanvs98 <darkcreationsl98@gmail.com>
2025-04-01 22:02:52 +0530

    mongoose connection implemented

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+try{
+    mongoose.connect('mongodb://127.0.0.1:27017/cpdp_db')
+    app.listen(3000,()=>{
+        console.log(`Server up & running on port 3000`);
+    })
+}catch (e) {
+    console.log(e);
+}
+app.get('/test-api',(req,resp)=>{
+    return resp.json({'message':'Hi the Server is Working....'})
+});
