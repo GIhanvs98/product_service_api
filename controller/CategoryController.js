01330a999fbf923872177cf75a4461657f424ae1
Gihanvs98 <darkcreationsl98@gmail.com>
2025-04-03 20:30:44 +0530

    create category function

--- controller/CategoryController.js
+++ controller/CategoryController.js
@@ -1,20 +1,53 @@
 const CategorySchema = require('../model/CategorySchema');
+// save (POST)
+const createCategory = (request, response) => {
+    const category = new CategorySchema({
+        // client side must send the file resource
+        // you must upload the icon into the S3 bucket and then you can get the response body.
 
-const createCategory = async (request, response)=>{
+        // the client send the ids of all the available countries, and the system must find all the countries for the request and save.
+
+        categoryName: request.body.categoryName,
+        icon: {
+            hash: 'Temp Hash',
+            resourceUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRr1TiZMtNq_El78BMq7-uS7g01qtuiAAVNj6tLspl1bAMw_t9AgZsxNlkrEzXrrYMGcz_S_pKEDq4FU-A_vW875CtAp1DHRfKZAt7xoww',
+            fileName: 'Temp File Name',
+            directory: 'Temp Directory'
+        }, // assume that you have send the image to the s3
+        availableCountries: [
+            {
+                countryId: 'Temp-Id-1',
+                countryName: 'Sri Lanka',
+            },
+            {
+                countryId: 'Temp-Id-2',
+                countryName: 'USA',
+            }
+        ],
+    });
+    category.save()
+        .then(result => {
+            response.status(201).json({code:201, message:'customer has been saved...', data:result});
+        }).catch(err => {
+        response.status(500).json({code:500, message:'something went wrong...', error:err});
+    })
+}
+// update (PUT)
+const updateCategory = (request, response) => {
     console.log(request.body);
 }
-const updateCategory = async (request, response)=>{
+// delete (DELETE)
+const deleteCategory = (request, response) => {
     console.log(request.body);
 }
-const deleteCategory = async (request, response)=>{
+// find by id (GET)
+const findCategoryById = (request, response) => {
     console.log(request.body);
 }
-const findCategoryById = async (request, response)=>{
+// find all (GET)
+const findAllCategories = (request, response) => {
     console.log(request.body);
 }
-const findAllCategories = async (request, response)=>{
-    console.log(request.body);
-}
-module.exports={
+module.exports = {
     createCategory, updateCategory, deleteCategory, findCategoryById, findAllCategories
 }
